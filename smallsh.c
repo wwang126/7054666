3cb856fe3d20caf1f62d9d24877d51fbd9acfe92
wwang126 <wavelet.wang@gmail.com>
2017-11-17 19:05:55 -0800

    returned execCmd to original

--- smallsh.c
+++ smallsh.c
@@ -83,33 +83,33 @@ char **parseInput(char* input, struct flagStruct * flags){
   flags->argCnt = i;
   return output;
 }
-// /*
-//  * Execute non-built in commands
-//  */
-// void execCmd(char** args, struct flagStruct * flags, char* cmdStatus){
-//     pid_t pid;
-//     int status = 0;
-//     printf("%s was sent in\n", args[0]);
-//     //Fork off child proccess for command
-//     pid = fork();
-//     //If child proccess
-//     if(pid == 0){
-//         if(execvp(*args, args) == -1){
-//             //if fails write to standard error
-//             fprintf(stderr, "Execute Failure\n");
-//             exit(EXIT_FAILURE);
-//         }
-//         printf("Executing cmd: %s", *args);
-//         fflush(stdout);
-//         exit(EXIT_SUCCESS);
-//     }
-//     //If fork failed
-//     if(pid == -1){
-//         //if fails write to standard error
-//         fprintf(stderr, "Fork Failure\n");
-//         exit(EXIT_FAILURE);
-//     }
-// }
+/*
+ * Execute non-built in commands
+ */
+void execCmd(char** args, struct flagStruct * flags, char* cmdStatus){
+    pid_t pid;
+    int status = 0;
+    printf("%s was sent in\n", args[0]);
+    //Fork off child proccess for command
+    pid = fork();
+    //If child proccess
+    if(pid == 0){
+        if(execvp(*args, args) == -1){
+            //if fails write to standard error
+            fprintf(stderr, "Execute Failure\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Executing cmd: %s", *args);
+        fflush(stdout);
+        exit(EXIT_SUCCESS);
+    }
+    //If fork failed
+    if(pid == -1){
+        //if fails write to standard error
+        fprintf(stderr, "Fork Failure\n");
+        exit(EXIT_FAILURE);
+    }
+}
 /*
  * Runs a list of commands from an array of strings
  */
@@ -145,28 +145,7 @@ void runCmd(char** args, struct flagStruct * flags, char* cmdStatus){
         }
         else{
             //Execute commands
-            pid_t pid;
-            int status = 0;
-            printf("%s was sent in\n", args[0]);
-            //Fork off child proccess for command
-            pid = fork();
-            //If child proccess
-            if(pid == 0){
-                if(execvp(*args, args) == -1){
-                    //if fails write to standard error
-                    fprintf(stderr, "Execute Failure\n");
-                    exit(EXIT_FAILURE);
-                }
-                printf("Executing cmd: %s", *args);
-                fflush(stdout);
-                exit(EXIT_SUCCESS);
-            }
-            //If fork failed
-            if(pid == -1){
-                //if fails write to standard error
-                fprintf(stderr, "Fork Failure\n");
-                exit(EXIT_FAILURE);
-            }
+            execCmd(args,flags,cmdStatus);
         }
     }
 }
